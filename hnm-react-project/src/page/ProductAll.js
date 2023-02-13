c2be32c48fba3c22346199bb91cb861e28b8c37e
guabaaa <gdandtop950923@gmail.com>
2023-02-13 22:15:52 +0900

    api 호출 연습

--- hnm-react-project/src/page/ProductAll.js
+++ hnm-react-project/src/page/ProductAll.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
-  return <div></div>;
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = "http://localhost:5000/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
 };
 
 export default ProductAll;
