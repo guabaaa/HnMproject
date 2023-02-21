9cd8f10d163c588262e06e9cb94a14410f37ebcc
guabaaa <gdandtop950923@gmail.com>
2023-02-21 19:04:03 +0900

    상품 불러오기

--- hnm-react-project/src/component/ProductCard.js
+++ hnm-react-project/src/component/ProductCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
     <div>
-      <img src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F76%2Fc6%2F76c682ca701aef10c1064840febd9a2c0513aa5e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]" />
+      <img src={item?.img} />
       <div>Conscious choice</div>
-      <div>벨티드 트윌 코트</div>
-      <div>₩99900</div>
-      <div>신제품</div>
+      <div>{item?.title}</div>
+      <div>₩{item?.price}</div>
+      <div>{item?.new == true ? "신제품" : " "}</div>
     </div>
   );
 };
--- hnm-react-project/src/page/ProductAll.js
+++ hnm-react-project/src/page/ProductAll.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
@@ -14,7 +15,15 @@ const ProductAll = () => {
   }, []);
   return (
     <div>
-      <ProductCard />
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3}>
+              <ProductCard item={menu} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
       <div></div>
     </div>
   );
