ee2eaf2280ce231899ad212735eae16e6cac3612
guabaaa <gdandtop950923@gmail.com>
2023-03-03 17:18:32 +0900

    효과변경

--- hnm-react-project/src/component/ProductCard.js
+++ hnm-react-project/src/component/ProductCard.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const ProductCard = ({ item }) => {
   return (
-    <div>
-      <img src={item?.img} />
-      <div>Conscious choice</div>
+    <div className="card-wrap">
+      <img src={item?.img} className="img-wrap" />
+      <div>{item?.choice == true ? "Conscious choice" : " "}</div>
       <div>{item?.title}</div>
       <div>₩{item?.price}</div>
       <div>{item?.new == true ? "신제품" : " "}</div>
