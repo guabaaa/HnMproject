1cc6e9ae4c07043bd258ea10fcbce953ba5745e1
guabaaa <gdandtop950923@gmail.com>
2023-03-26 20:02:15 +0900

    private route

--- hnm-react-project/src/App.js
+++ hnm-react-project/src/App.js
@@ -6,6 +6,8 @@ import ProductDetail from "./page/ProductDetail";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import Navbar from "./component/Navbar";
+import { useEffect } from "react";
+import PrivateRoute from "./route/PrivateRoute";
 
 // 1. 전체상품페이지, 로그인페이지, 상품상세페이지
 // 2. 전체상품 페이지에서는 전체 상품을 볼 수 있다.
@@ -18,13 +20,22 @@ import Navbar from "./component/Navbar";
 // 7. 상품을 검색할 수 있다.
 function App() {
   const [authenticate, setAuthenticate] = useState(false); //true면 로그인이 됨, false면 로그인이 안됨
+  useEffect(() => {
+    console.log("Aaaa", authenticate);
+  }, [authenticate]);
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route
+          path="/login"
+          element={<Login setAuthenticate={setAuthenticate} />}
+        />
+        <Route
+          path="/product/:id"
+          element={<PrivateRoute authenticate={authenticate} />}
+        />
       </Routes>
     </div>
   );
--- hnm-react-project/src/component/ProductCard.js
+++ hnm-react-project/src/component/ProductCard.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const showDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
   return (
-    <div className="card-wrap">
+    <div className="card-wrap" onClick={showDetail}>
       <img src={item?.img} className="img-wrap" />
       <div>{item?.choice == true ? "Conscious choice" : " "}</div>
       <div>{item?.title}</div>
